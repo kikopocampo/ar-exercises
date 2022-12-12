9586a046b9c6eb4ea0ef283786a259173c92ddc9
Francisco <kikopocampo@gmail.com>
2022-12-12 16:07:15 -0700

    add employee data

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Total Revenue: #{Store.sum('annual_revenue')}"
+puts "Average Revenue: #{Store.sum('annual_revenue')/Store.count}"
+puts Store.where('annual_revenue >= 1000000').count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Bob", last_name: "Brown", hourly_rate: 25)
+@store1.employees.create(first_name: "Alice", last_name: "Andrew", hourly_rate: 15)
+@store1.employees.create(first_name: "Carol", last_name: "Charles", hourly_rate: 15)
+@store1.employees.create(first_name: "Dennis", last_name: "Danby", hourly_rate: 15)
+@store1.employees.create(first_name: "Elijah", last_name: "Endy", hourly_rate: 15)
+
+@store2.employees.create(first_name: "Charles", last_name: "Chamberlain", hourly_rate: 35)
+@store2.employees.create(first_name: "Aska", last_name: "Brown", hourly_rate: 15)
+@store2.employees.create(first_name: "Charlie", last_name: "Clad", hourly_rate: 15)
+@store2.employees.create(first_name: "Donny", last_name: "Franklin", hourly_rate: 15)
+@store2.employees.create(first_name: "Ella", last_name: "Washer", hourly_rate: 15)
+p Employee.count
