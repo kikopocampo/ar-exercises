df5a1e8a79dff0d66f5f675258de04c83e492b8a
Francisco <kikopocampo@gmail.com>
2022-12-12 15:19:02 -0700

    implement exercise 1-3

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create do |x|
+  x.name = "Burnaby"
+  x.annual_revenue = 300000
+  x.mens_apparel = true
+  x.womens_apparel = true
+end
+
+Store.create do |x|
+  x.name = "Richmond"
+  x.annual_revenue = 1260000
+  x.mens_apparel = false
+  x.womens_apparel = true
+end
+
+Store.create do |x|
+  x.name = "Gastown"
+  x.annual_revenue = 190000
+  x.mens_apparel = true
+  x.womens_apparel = false
+end
+
+puts Store.count
